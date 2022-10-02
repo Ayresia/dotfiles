4adbf270725ee06da99efac51609e4d5ada3d5d0
Ayresia <nalizyoncatania0@gmail.com>
2022-10-02 14:06:53 +0200

    feat(nvim): auto install plugins

--- configs/nvim/.config/nvim/lua/ayresia/set.lua
+++ configs/nvim/.config/nvim/lua/ayresia/set.lua
@@ -20,5 +20,8 @@ vim.opt.updatetime = 50
 
 vim.g.mapleader = " "
 
-vim.g.moonflyCursorColor = true
-vim.cmd("colorscheme moonfly")
+local themeLoaded, _ = pcall(vim.cmd, "colorscheme moonfly")
+
+if themeLoaded then
+    vim.g.moonflyCursorColor = true
+end
