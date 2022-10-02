85fb743b973e7c3f3ac206971f645ad9e6e1399b
Ayresia <nalizyoncatania0@gmail.com>
2022-10-02 17:30:48 +0200

    feat(nvim): add treesitter

--- configs/nvim/.config/nvim/lua/ayresia/packer.lua
+++ configs/nvim/.config/nvim/lua/ayresia/packer.lua
@@ -2,4 +2,9 @@ return require("packer").startup(function()
     use("wbthomason/packer.nvim")
     use("bluz71/vim-moonfly-colors")
     use("bluz71/vim-mistfly-statusline")
+    use("nvim-treesitter/nvim-treesitter", {
+        run = function()
+            require("nvim-treesitter.install").update({ with_sync = true })
+        end
+    })
 end)
