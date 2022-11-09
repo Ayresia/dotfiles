ce6ed56c36bb301d46abd93516b1b9e33703bded
Ayresia <nalizyoncatania0@gmail.com>
2022-11-09 09:11:49 +0100

    feat(lsp): bind c-space to confirm option

--- configs/nvim/.config/nvim/after/plugin/lsp.lua
+++ configs/nvim/.config/nvim/after/plugin/lsp.lua
@@ -11,7 +11,7 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-Space>"] = cmp.mapping.confirm(),
         ["<C-e>"] = cmp.mapping.abort(),
     }),
     sources = cmp.config.sources({
