92555a1c072a92c74e402062281883caf1b52406
Ayresia <nalizyoncatania0@gmail.com>
2022-12-21 09:02:12 +0100

    feat(nvim): remove wrap, add incremental search

--- configs/nvim/.config/nvim/lua/ayresia/set.lua
+++ configs/nvim/.config/nvim/lua/ayresia/set.lua
@@ -10,10 +10,13 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
 vim.opt.smartindent = true
 
 vim.opt.hlsearch = false
-vim.opt.wrap = true
+vim.opt.incsearch = true
+
+vim.opt.wrap = false
 
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 50
